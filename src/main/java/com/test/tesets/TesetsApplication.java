1237acf8b75afdc5f86216d8316beb37a3cc4b4c
Mporras <t-mporrasm@wingo.com>
2023-02-10 19:59:16 -0500

    initial commit

---
+++ src/main/java/com/test/tesets/TesetsApplication.java
@@ -0,0 +1,13 @@
+package com.test.tesets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesetsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/tesets/TesetsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.tesets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
